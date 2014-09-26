fba2c68506f96112af4bf35a0dba490c4af4e339
Ignacio Tartavull <tartavull@gmail.com>
2014-09-26 16:26:27 -0300

    uploading images

--- app/controller/kernel.js
+++ app/controller/kernel.js
@@ -2,25 +2,45 @@ window.ipython = window.ipython || {};
 
 var proxy = require("ipython-notebook-proxy");
 var path = require('path');
+var portfinder = require('portfinder');
+var fs = require('fs');
+var gui = require('nw.gui');
+var win = gui.Window.get();
 
 ipython.init = function(argv, callback) {
 
-	var pattern = /--notebook=((\/|\\\\).*)*/;
-	var filepath = pattern.exec(argv[0]);
-	var dirname = path.dirname(filepath[1]);
+	portfinder.getPort(function(err, port) {
 
-	if (dirname) {
+		var filename = "";
 		var urlpath = "inotebook";
-		var port = 10001;
-		console.log("Opening file : " + dirname);
-		proxy.start(dirname, urlpath, port);
-	}
+		var dirname = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+
+		var home = true;
+		if (argv.length > 0) {
+			var pattern = /--notebook=((\/|\\\\).*)*/;
+			var filepath = pattern.exec(argv[0]);
+			if (filepath[1]) {
+				if (fs.lstatSync(filepath[1]).isFile()) {
+
+					var dirname = path.dirname(filepath[1]);
+					var filename = path.basename(filepath[1]);
+					urlpath = urlpath + "/notebooks/" + filename + "#";
+
+			
+					win.maximize();
+				}
+			}
+		}
 
 
-	win.on('closed', function() {
-		console.log("Stoping server");
-		proxy.stop_all();
+		proxy.start(dirname, "inotebook", port);
+
+		win.on('closed', function() {
+			console.log("Stoping server");
+			proxy.stop_all();
+		});
+
+		callback(urlpath, port);
+
 	});
-
-	callback(urlpath, port);
 };
